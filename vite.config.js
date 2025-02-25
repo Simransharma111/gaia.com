af6837d3572c35b0fee65e159b252d039ce885af
Simransharma111 <157696421+Simransharma111@users.noreply.github.com>
2025-02-25 22:26:01 +0530

    Update vite.config.js
    add url

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,10 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-
 export default defineConfig({
-  plugins: [react()],
   server: {
-    host: '0.0.0.0',  // Ensure it binds to all network interfaces
-    port: 5173,  // Default Vite port, but Render will override this
-  },
+    host: true, // This makes the app accessible externally
+    port: 5173,
+    strictPort: true,
+    preview: {
+      allowedHosts: ["https://gaia-com.onrender.com/"] // Add your Render domain here
+    }
+  }
 });
